50c20283652376263a0f9ded1bc629195e964158
ramiro <lidnele@hotmail.com>
2016-04-04 13:57:52 -0300

    test

---
+++ package.js
@@ -0,0 +1,26 @@
+Package.describe({
+  summary: "Parse a document",
+  name: 'aida:resume-parser',
+  version: '0.0.10',
+  documentation: null
+});
+
+Npm.depends({
+  'combined-stream': "0.0.4",
+  'util': "0.10.3",
+  //'path'
+  //'http'
+  //'https'
+  'url': "0.7.9",
+  'mime': "1.2.11",
+  'async': "0.6.2"
+});
+
+
+Package.on_use(function (api, where) {
+  api.versionsFrom('METEOR@1.1.0.2');
+  api.use('underscore@1.0.3', 'server');
+  api.addFiles('lib/node-form-data/lib/form_data.js', 'server');  
+  api.addFiles('main.js', 'server');
+  api.export("ResumeParser", 'server');
+});
